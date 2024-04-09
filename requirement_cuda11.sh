b6329a2f8ef217625a9856f3f3d62cdd7f6e8ded
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2024-04-09 11:18:53 +0900

    add cuda 11 docker files.

---
+++ requirement_cuda11.sh
@@ -0,0 +1,8 @@
+pip install torchvision==0.8.0
+pip install tqdm
+pip install pandas
+pip install kornia==0.1.4.post2
+pip install opencv-python
+pip install scipy==1.2.0
+pip install jupyterlab
+pip install matplotlib
