a79d64cec758089338ed187b0fe22c5d924b5291
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2024-04-18 16:25:24 +0900

    coarse aligh in align_2images.py

--- quick_start/align_2images.py
+++ quick_start/align_2images.py
@@ -26,6 +26,8 @@ def align2imgs(args):
     # Load input images
     img1 = Image.open(args.img1).convert('RGB')
     img2 = Image.open(args.img2).convert('RGB')
+
+    # save for debug
     plt.figure(figsize=(20, 10))
     plt.subplot(1, 2, 1)
     plt.imshow(img1)
@@ -40,22 +42,54 @@ def align2imgs(args):
     plt.savefig(args.outdir + 'input_images.png')
 
     # Load the model
-    resumePth = args.resumePth
-    kernelSize = 7
     Transform = outil.Homography
-    nbPoint = 4
     network = {'netFeatCoarse' : model.FeatureExtractor(), 
-               'netCorr'       : model.CorrNeigh(kernelSize),
-               'netFlowCoarse' : model.NetFlowCoarse(kernelSize), 
-               'netMatch'      : model.NetMatchability(kernelSize),
+               'netCorr'       : model.CorrNeigh(args.kernelSize),
+               'netFlowCoarse' : model.NetFlowCoarse(args.kernelSize), 
+               'netMatch'      : model.NetMatchability(args.kernelSize),
                }
     for key in list(network.keys()) : 
         network[key].cuda()
         typeData = torch.cuda.FloatTensor
 
-    pass
+    # Load weights
+    param = torch.load(args.resumePth)
+    for key in list(param.keys()) : 
+        network[key].load_state_dict( param[key] ) 
+        network[key].eval()
 
-# in case called as a script
+    # coarse alignment
+    coarseModel = CoarseAlign(args.nbScale, args.coarseIter, args.coarsetolerance, 'Homography'
+                              , args.minSize, segId = 1, segFg = True, imageNet = True, scaleR = args.scaleR)
+    coarseModel.setSource(img1)
+    coarseModel.setTarget(img2)
+    img2w, img2h = coarseModel.It.size # It: image target.
+    feat_tensor = F.normalize(network['netFeatCoarse'](coarseModel.ItTensor))
+    # grid
+    gridX = torch.linspace(-1, 1, steps = img2w).view(1, 1, -1, 1).expand(1, img2h, img2w, 1)
+    gridY = torch.linspace(-1, 1, steps = img2h).view(1, -1, 1, 1).expand(1, img2h, img2w, 1)
+    grid = torch.cat((gridX, gridY), dim = 3).cuda()
+    warper = tgm.HomographyWarper(img2h, img2w)
+    # compute best parameters
+    bestPrm, inlierMask = coarseModel.getCoarse(np.zeros((img2h, img2w)))
+    bestPrm = torch.from_numpy(bestPrm).unsqueeze(0).cuda()
+    flowCoarse = warper.warp_grid(bestPrm)
+    img1_coarse = F.grid_sample(coarseModel.IsTensor, flowCoarse) #Is: image source.
+    img1_coarse_pil = transforms.ToPILImage()(img1_coarse.cpu().squeeze())
+
+    # save for debug
+    plt.figure(figsize=(20, 10))
+    plt.subplot(1, 2, 1)
+    plt.axis('off')
+    plt.title('Source Image (Coarse)')
+    plt.imshow(img1_coarse_pil)
+    plt.subplot(1, 2, 2)
+    plt.axis('off')
+    plt.title('Target Image')
+    plt.imshow(img2)
+    plt.show()
+    plt.savefig(args.outdir + 'coarse_alignment.png')
+
 if __name__ == '__main__':
     # get arguments with default values.
     parser = argparse.ArgumentParser(description='Align two images')
@@ -63,6 +97,14 @@ if __name__ == '__main__':
     parser.add_argument('--img2', type=str, help='path to the second image', default='../img/ArtMiner_Detail_Res13_11.png')
     parser.add_argument('--outdir', type=str, help='path to the output folder', default='../output/')
     parser.add_argument('--resumePth', type=str, help='path to the model', default='../model/pretrained/MegaDepth_Theta1_Eta001_Grad1_0.774.pth')
+    parser.add_argument('--kernelSize', type=int, help='size of the kernel', default=7)
+    parser.add_argument('--nbPoint', type=int, help='number of points to use for alignment', default=4)
+    parser.add_argument('--nbScale', type=int, help='number of scales to use for alignment', default=7)
+    parser.add_argument('--coarseIter', type=int, help='number of iterations for coarse alignment', default=10000)
+    parser.add_argument('--coarsetolerance', type=float, help='tolerance for coarse alignment', default=0.05)
+    parser.add_argument('--minSize', type=int, help='minimum size for the image', default=400)
+    parser.add_argument('--scaleR', type=float, help='scale ratio', default=1.2)
+
     args = parser.parse_args()
 
     align2imgs(args)
